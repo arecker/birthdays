0814b5ca47da8bb00b23a2197f8bc28f4e23ddba
Alex Recker <alex@reckerfamily.com>
2021-10-29 22:23:18 -0500

    BANG BANG BANG

--- birthdays.py
+++ birthdays.py
@@ -1,9 +1,14 @@
 import configparser
 import datetime
+import json
 import logging
 import os
 import platform
+import random
 import sys
+import urllib.error
+import urllib.parse
+import urllib.request
 
 logging.basicConfig(stream=sys.stderr,
                     level=logging.INFO,
@@ -16,6 +21,50 @@ def load_config(path=''):
     logging.info('loaded %d section(s) from config %s', len(config.sections()),
                  path)
     return config
+
+
+def build_message(name, message_url):
+    variations = [
+        'Hey, happy birthday to {}!',
+        'HAPPY BIRTHDAY {}!!!',
+        '{}.  IT IS YOUR BIRTHDAY.',
+        'Guess who was born today.  {}, that\'s who.',
+        'Hey everyone, let\'s wish {} a happy birthday!',
+    ]
+    message = random.choice(variations).format(name)
+    message += '\n' + message_url
+    return message
+
+
+def post(name, section, encoding='utf-8'):
+    headers = {'Content-type': 'application/json'}
+    data = {
+        'username': section['username'],
+        'channel': section['channel'],
+        'icon_emoji': section['emoji'],
+        'text': build_message(name=name, message_url=section['message_url']),
+    }
+    data = json.dumps(data)
+    data = data.encode(encoding)
+    request = urllib.request.Request(headers=headers,
+                                     url=section['webhook_url'],
+                                     data=data)
+    try:
+        response = urllib.request.urlopen(request)
+        response_data = response.read().decode(encoding)
+        response_data = json.loads(data)
+        logging.info('webhook returned HTTP: %d %s, data: %s', response.code,
+                     response.reason, response_data)
+    except urllib.error.HTTPError as e:
+        error_data = e.read().decode(encoding)
+
+        try:
+            error_data = json.loads(error_data)
+        except json.decoder.JSONDecodeError:
+            error_data = '"' + error_data + '"'
+
+        logging.error('webhook returned HTTP %d: %s, data: %s', e.code,
+                      e.reason, error_data)
 
 
 def main():
@@ -40,6 +89,7 @@ def main():
 
         if is_today:
             logging.info('building birthday message for %s', name)
+            post(name=name, section=info)
         else:
             logging.info('skipping %s, not birthday yet', name)
 
